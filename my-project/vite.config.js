9c9cf211f89519e6dfe383b25f7a28168b89ed5a
kowshik <kowshikvalpireddy@gmail.com>
2024-03-17 16:31:24 +0530

    dfgdgdf

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,27 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { viteCors } from 'vite-plugin-cors';
 
 export default defineConfig({
   server: {
     proxy: {
-      '/server': {
-        target: 'http://localhost:6000',
-        secure: false
-      }
-    }
+      '/api': 'https://mern-postcrafts.vercel.app/', // Maps requests to /api to your actual API server
+    },
   },
-  plugins: [
-    react(),
-    viteCors({
-      origin: '*',
-      methods: 'GET,POST,PUT,DELETE,OPTIONS',
-      allowedHeaders: 'Content-Type,Authorization,X-Requested-With',
-      exposedHeaders: '',
-      credentials: false,
-      maxAge: 86400,
-      preflightContinue: false,
-      optionsSuccessStatus: 204
-    })
-  ]
+  // ... other configurations
 });
