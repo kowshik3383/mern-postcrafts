ffc2f9762d8edbfdbd174ba804259df271befc54
kowshik <kowshikvalpireddy@gmail.com>
2024-03-17 19:11:45 +0530

    Add new feature XYZ

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:6000/', // Maps requests to /api to your actual API server
+      '/api': {
+        target: 'http://localhost:6000',
+        secure: false,
+      },
     },
   },
-  // ... other configurations
+
+  plugins: [react()],
 });
