0bf2c08545de7b22dfb52be5dfb86a362b1a927a
kowshik <kowshikvalpireddy@gmail.com>
2024-03-17 16:18:03 +0530

    dfdfgdg

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,15 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { viteCors } from 'vite-plugin-cors';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/server':{
-        target:'http://localhost:6000',
-        secure:false
-      },
-    },
+  server: {
+    proxy: {
+      '/server': {
+        target: 'http://localhost:6000',
+        secure: false
+      }
+    }
   },
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteCors({
+      origin: '*',
+      methods: 'GET,POST,PUT,DELETE,OPTIONS',
+      allowedHeaders: 'Content-Type,Authorization,X-Requested-With',
+      exposedHeaders: '',
+      credentials: false,
+      maxAge: 86400,
+      preflightContinue: false,
+      optionsSuccessStatus: 204
+    })
+  ]
+});
